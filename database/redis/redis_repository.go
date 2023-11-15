6ac43c801f3b29b6d082015a1ed96a8e4391a82c
cxw <leor_cao@163.com>
2023-11-15 17:24:44 +0800

    mod: 修改Redis基础Repository的名车给你

--- database/redis/redis_repository.go
+++ database/redis/redis_repository.go
@@ -2,27 +2,27 @@ package redis
 
 import "time"
 
-type Repository struct {
+type RedisRepository struct {
 	RDB *Client `inject:""`
 }
 
-func (repo *Repository) Set(key string, value interface{}, expire int64) error {
+func (repo *RedisRepository) Set(key string, value interface{}, expire int64) error {
 	return repo.RDB.Set(key, value, time.Duration(expire)).Err()
 }
 
-func (repo *Repository) Get(key string) (string, error) {
+func (repo *RedisRepository) Get(key string) (string, error) {
 	return repo.RDB.Get(key).Result()
 }
 
-func (repo *Repository) Del(keys ...string) error {
+func (repo *RedisRepository) Del(keys ...string) error {
 	return repo.RDB.Del(keys...).Err()
 }
 
-func (repo *Repository) Expire(key string, expire int64) error {
+func (repo *RedisRepository) Expire(key string, expire int64) error {
 	return repo.RDB.Expire(key, time.Duration(expire)).Err()
 }
 
-func (repo *Repository) Exists(key string) (bool, error) {
+func (repo *RedisRepository) Exists(key string) (bool, error) {
 	exist, err := repo.RDB.Exists(key).Result()
 	if err != nil {
 		return false, err
