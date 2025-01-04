d0cf7b8327aae6de5676d70950e02fa0e6ffb2e6
leor <leorqao@gmail.com>
2025-01-04 21:02:46 +0800

    mod: 修改微信公众号相关

--- plugin/wechat/official_account.go
+++ plugin/wechat/official_account.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/silenceper/wechat/v2/officialaccount/oauth"
+	"github.com/spf13/cast"
 
 	"github.com/leor-w/kid/utils"
 )
@@ -17,11 +18,21 @@ func (w *Wechat) BuildOauthURL(redirectURL, scope string) (string, error) {
 			return "", err
 		}
 	}
-	return w.official.GetOauth().GetRedirectURL(redirectURL, scope, state)
+	return w.OfficialAccount().GetOauth().GetRedirectURL(redirectURL, scope, state)
 }
 
 // GetOAuthUserInfo 获取微信用户信息
-func (w *Wechat) GetOAuthUserInfo(code string) (*oauth.UserInfo, error) {
+func (w *Wechat) GetOAuthUserInfo(code, state string) (*oauth.UserInfo, error) {
+	if w.cache != nil {
+		key := getWechatOauthStateKey(state)
+		rest := w.cache.Get(key)
+		if rest == nil {
+			return nil, fmt.Errorf("微信公众号 获取用户信息错误: %w", fmt.Errorf("state 无效"))
+		}
+		if cast.ToString(rest) != state {
+			return nil, fmt.Errorf("微信公众号 获取用户信息错误: %w", fmt.Errorf("state 无效"))
+		}
+	}
 	res, err := w.OfficialAccount().GetOauth().GetUserAccessToken(code)
 	if err != nil {
 		return nil, err
